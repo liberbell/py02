b3d8033810749ba7af2e8ea8bcfa82f3614ad6fa
Liberbell <liberbell@gmail.com>
2019-05-15 08:42:33 +0900

    urllibs4

--- urllibs.py
+++ urllibs.py
@@ -4,7 +4,7 @@ def main():
     url = 'https://httpbin.org/xml'
 
     result = urllib.request.urlopen(url)
-    print(result.status)
+    print('Result code: {0}'.format(result.status))
 
 if __name__ == '__main__':
     main()
