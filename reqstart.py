8799dafbbd63c2f58e032c159cbb76548bc7133a
Liberbell <liberbell@gmail.com>
2019-05-17 08:01:02 +0900

    reqstart2

---
+++ reqstart.py
@@ -0,0 +1,14 @@
+import reqests
+
+def main():
+    url =
+
+
+def printResult(resData):
+    print('Result code: {0}'.format(resData.status_code))
+    print('\n')
+
+    print('Headers ------------')
+
+if __name__ == '__main__':
+    main()
