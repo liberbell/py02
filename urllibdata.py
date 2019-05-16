d500c38dbb26a00f6fd19413c7a57e7df9abac62
Liberbell <liberbell@gmail.com>
2019-05-16 18:05:48 +0900

    urllibdata4

--- urllibdata.py
+++ urllibdata.py
@@ -1,4 +1,5 @@
 import urllib.request
+import urllib.parse
 
 def main():
     url = 'https://httpbin.org/get'
@@ -8,6 +9,8 @@ def main():
         "is_author": True
     }
 
+    data = urllib.parse.urlencode(args)
+
     result = urllib.request.urlopen(url)
 
     print('Result code: {0}'.format(result.status))
@@ -15,7 +18,7 @@ def main():
     print('Headers ------------')
 
     print('Returned data ------')
-
+    print(result.read().decode('UTF-8'))
 
 if __name__ == '__main__':
     main()
