d5199745e17406607464137cd5bbe77ef34b4bf0
Liberbell <liberbell@gmail.com>
2019-05-17 07:52:27 +0900

    urlliberr9

--- urlliberr.py
+++ urlliberr.py
@@ -1,6 +1,6 @@
 import urllib.request
 from http import HTTPStatus
-from urllib.error import HTTPError
+from urllib.error import HTTPError, URLError
 
 def main():
     # url = 'https://httpbin.org/html'
@@ -14,6 +14,9 @@ def main():
             print(result.read().decode('UTF-8'))
     except HTTPError as err:
         print('Error: {0}'.format(err.code))
+
+    except URLError as err:
+        print('That server is bunk. {0}'.format(err.reason))
 
 if __name__ == '__main__':
     main()
