40e07868033195e84c2a846dba6d813a3ea4584f
Liberbell <liberbell@gmail.com>
2019-05-17 07:49:33 +0900

    urlliberr7

--- urlliberr.py
+++ urlliberr.py
@@ -7,10 +7,13 @@ def main():
     url = 'https://httpbin.org/status/404'
     # url = 'https://no-such-server.org'
 
-    result = urllib.request.urlopen(url)
-    print('Result code: {0}'.format(result.status))
-    if (result.getcode() == HTTPStatus.OK):
-        print(result.read().decode('UTF-8'))
+    try:
+        result = urllib.request.urlopen(url)
+        print('Result code: {0}'.format(result.status))
+        if (result.getcode() == HTTPStatus.OK):
+            print(result.read().decode('UTF-8'))
+    except HTTPError as err:
+        print('Error: {0}'.format(err.code))
 
 if __name__ == '__main__':
     main()
