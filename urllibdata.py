a7a50956fd8206db47818317cd4b3bb87dd6527c
Liberbell <liberbell@gmail.com>
2019-05-16 18:02:23 +0900

    urllibdata2

--- urllibdata.py
+++ urllibdata.py
@@ -3,8 +3,15 @@ import urllib.request
 def main():
     url = 'https://httpbin.org/get'
 
+
+    result = urllib.request.urlopen(url)
+
     print('Result code: {0}'.format(result.status))
-    
+
+    print('Headers ------------')
+
+    print('Returned data ------')
+
 
 if __name__ == '__main__':
     main()
