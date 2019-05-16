5821df63e0c1b7c3603c85e1e71a036c13927601
Liberbell <liberbell@gmail.com>
2019-05-17 07:38:55 +0900

    urlliberr2

---
+++ urlliberr.py
@@ -0,0 +1,14 @@
+import urllib.request
+
+def main():
+    url = 'https://httpbin.org/html'
+    url = 'https://httpbin.org/status/404'
+    url = 'https://no-such-server.org'
+
+    result = urllib.request.urlopen(url)
+    print('Result code: {0}'.format(resutl.status))
+    if (result.getcode() == 200):
+        print(result.read().decode('UTF-8'))
+
+if __name__ == '__main__':
+    main()
