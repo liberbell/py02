d3d6b2b4b4abcb3be254a4855a2ecd845dfabb37
Liberbell <liberbell@gmail.com>
2019-05-16 17:53:10 +0900

    urllibs1

--- urllibs.py
+++ urllibs.py
@@ -6,5 +6,9 @@ def main():
     result = urllib.request.urlopen(url)
     print('Result code: {0}'.format(result.status))
 
+    print('Headers ------------')
+
+    print('Returned data ------')
+
 if __name__ == '__main__':
     main()
