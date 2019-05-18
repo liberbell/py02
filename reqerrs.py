5e8dcf363eb0e4c95e2b8a9899d762761a5958b7
Liberbell <liberbell@gmail.com>
2019-05-18 09:18:28 +0900

    reqerrs4

--- reqerrs.py
+++ reqerrs.py
@@ -13,9 +13,9 @@ def printResult(resData):
     print('Result code: {0}'.format(resData.status_code))
     print('\n')
 
-    print('Headers ------------')
-    print(resData.headers)
-    print('\n')
+    # print('Headers ------------')
+    # print(resData.headers)
+    # print('\n')
 
     print('Returned data ------')
     print(resData.text)
