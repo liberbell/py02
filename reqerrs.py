24cde78cda863aec40b8ae554c8686e81f5026be
Liberbell <liberbell@gmail.com>
2019-05-18 09:13:47 +0900

    reqerrs2

--- reqerrs.py
+++ reqerrs.py
@@ -1,25 +1,10 @@
 import requests
 
 def main():
-    url = 'https://httpbin.org/xml'
+    url = 'https://httpbin.org/status/404'
     result = requests.get(url)
-    # printResult(result)
-
-    url = 'https://httpbin.org/post'
-    dataValues = {
-        "key1": "value1",
-        "key2": "value2",
-        "key3": "value3"
-    }
-    result = requests.post(url, data=dataValues)
-    # printResult(result)
-
-    url = 'https://httpbin.org/get'
-    headerValues = {
-        "User-Agent": "Neo Tokyo App / 1.0.0"
-    }
-    result = requests.get(url, headers=headerValues)
     printResult(result)
+
 
 def printResult(resData):
     print('Result code: {0}'.format(resData.status_code))
