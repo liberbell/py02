3e3facc8047edfb36ac76626279668ad34cd2eda
Liberbell <liberbell@gmail.com>
2019-05-20 08:45:09 +0900

    jsonserialize3

--- jsonserialize.py
+++ jsonserialize.py
@@ -11,13 +11,7 @@ def main():
         "price": 8.99
     }
 
-    data = json.loads(jsonStr)
-
-    print('Sandwich: ' + data['sandwich'])
-    if (data['toasted']):
-        print('And it`s toasted!')
-    for topping in data['toppings']:
-        print('Topping: ' + topping)
+    print('Json data: -------------')
 
 if __name__ == '__main__':
     main()
