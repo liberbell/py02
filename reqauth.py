6d587b5eb2a9bf6243e9856693177c4d4a7f21b1
Liberbell <liberbell@gmail.com>
2019-05-19 11:51:21 +0900

    reqauth7

--- reqauth.py
+++ reqauth.py
@@ -8,7 +8,7 @@ def main():
 
     myCreds = HTTPBasicAuth('root', 'pass')
 
-    result = requests.get(url, auth=('root', 'pass'))
+    result = requests.get(url, auth=('root', 'pass1'))
 
     printResult(result)
     # try:
