686196b3d2dc97afca8e2ed2c985874e1b6d6cde
Liberbell <liberbell@gmail.com>
2019-05-19 11:40:04 +0900

    reqauth1

---
+++ reqauth.py
@@ -0,0 +1,28 @@
+import requests
+from requests import HTTPError, Timeout
+
+def main():
+    # url = 'https://httpbin.org/status/404'
+    url = 'https://httpbin.org/delay/5'
+    try:
+        result = requests.get(url, timeout=2)
+        result.raise_for_status()
+        printResult(result)
+    except HTTPError as err:
+        print('Error: {0}'.format(err))
+    except Timeout as err:
+        print('Request timeout error: {0}',format(err))
+
+def printResult(resData):
+    print('Result code: {0}'.format(resData.status_code))
+    print('\n')
+
+    # print('Headers ------------')
+    # print(resData.headers)
+    # print('\n')
+
+    print('Returned data ------')
+    print(resData.text)
+
+if __name__ == '__main__':
+    main()
