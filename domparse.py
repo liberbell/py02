d1bf0c8ce6b2f9b412b9262fc77c072543159381
Liberbell <liberbell@gmail.com>
2019-05-22 07:47:33 +0900

    domparse3

--- domparse.py
+++ domparse.py
@@ -5,6 +5,11 @@ def main():
     url = 'https://httpbin.org/xml'
     result = requests.get(url)
 
+    domtree = xml.dom.minidom.parseString(result.text)
+    rootnode = domtree.documentElement
+
+    print('The Root element is {0}'.format(rootnode.nodeName))
+
 
 if __name__ == '__main__':
     main()
