293b6ef8ea5ad11bb082c87cd0d6b456c431e4a0
Liberbell <liberbell@gmail.com>
2019-05-23 07:36:43 +0900

    lxmlstart3

--- lxmlstart.py
+++ lxmlstart.py
@@ -4,3 +4,10 @@ from lxml import etree
 def main():
     url = 'https://httpbin.org/xml'
     resutl = requests.get(url)
+
+    doc = etree.fromstrint(result.content)
+    print(result.text)
+
+
+if __name__ == '__main__':
+    main()
